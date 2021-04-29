a6c45cc654df6a925c2fd5add4528e267d686436
Ahmed Hirsi <ahirsi61@gmail.com>
2021-04-29 17:42:48 -0500

    generated password only using lowwercase characters

--- script.js
+++ script.js
@@ -15,18 +15,30 @@ console.log("Password length= ", length);
   console.log("include lowercase= ", includelowercase);
   let passwordCanInclude = "";
   let allLowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+  let mypassword = "";
+  let lengthOfPasswordAlreadyBuilt = 0;
   console.log("passwordCanInclude", passwordCanInclude);
   if (includelowercase) {
     passwordCanInclude = passwordCanInclude + allLowercaseCharacters;
+    mypassword = mypassword + allLowercaseCharacters[Math.floor(Math.random() * allLowercaseCharacters.length)];
+    lengthOfPasswordAlreadyBuilt = lengthOfPasswordAlreadyBuilt + 1;
+    //  lengthOfPasswordAlreadyBuilt += 1;
   }
   console.log("passwordCanInclude", passwordCanInclude);
-  
-  let includeuppercase = confirm("Do you want to include upper case characters in your password? Click Ok for yes and cancel for no");
-  console.log("include uppercase= ", includeuppercase);
-  let includespecialcharacters = confirm("Do you want to include special characters in your password? Click Ok for yes and cancel for no");
-  console.log("include special characters= ", includespecialcharacters);
-  let includenumeric = confirm("Do you want to include numeric characters in your password? Click Ok for yes and cancel for no");
-  console.log("include Numeric= ", includenumeric);
+  console.log("mypassword", mypassword);
+
+  // let includeuppercase = confirm("Do you want to include upper case characters in your password? Click Ok for yes and cancel for no");
+  // console.log("include uppercase= ", includeuppercase);
+  // let includespecialcharacters = confirm("Do you want to include special characters in your password? Click Ok for yes and cancel for no");
+  // console.log("include special characters= ", includespecialcharacters);
+  // let includenumeric = confirm("Do you want to include numeric characters in your password? Click Ok for yes and cancel for no");
+  // console.log("include Numeric= ", includenumeric);
+
+  for (let index = 0; index < (length - lengthOfPasswordAlreadyBuilt); index++) {
+    mypassword = mypassword + passwordCanInclude[Math.floor(Math.random() * passwordCanInclude.length)];
+    
+  }
+  return mypassword;
 }
 // Write password to the #password input
 function writePassword() {
